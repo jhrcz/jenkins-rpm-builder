09d1cefcd39f59c645df5fe32bd91aa9c6f0fa27
Jan Horacek (jahor/jhrcz) <jahor@jhr.cz>
2012-10-22 18:59:07 +0200

    vcs si vytahuje jenkins

--- buildscript-tito.sh
+++ buildscript-tito.sh
@@ -2,22 +2,22 @@
 set -x
 set -e
 
-rm -rf rctc-repo
+#rm -rf *-repo
 rm -rf repo
+rm -rf tmp-tito
 
 mkdir -p repo
+mkdir -p tmp-tito
 
-git clone /opt/rctc-repo/
-cd  rctc-repo
+[ -z "$MOCK_BUILDER" ] && MOCK_BUILDER="$1" || true
+#[ -z "$MOCK_BUILDER" ] && MOCK_BUILDER="epel-6-x86_64" || true
 
 # override path to use mock from /usr/bin and not /usr/sbin
 export PATH=/usr/bin:$PATH
 
 # move reulting packages in one directory for next steps
-mkdir tmp-tito
-tito build -o tmp-tito --builder mock --builder-arg mock=epel-6-x86_64 --rpm
+tito build --debug -o tmp-tito --builder mock --builder-arg mock=$MOCK_BUILDER --rpm
 
 # create repository (all files from this repo should be saved as artifacts)
-find tmp-tito/ -maxdepth 1 -type f -exec mv '{}' ../repo/ \;
-createrepo ../repo
-
+find tmp-tito/ -maxdepth 1 -type f -exec mv '{}' repo/ \;
+createrepo repo
